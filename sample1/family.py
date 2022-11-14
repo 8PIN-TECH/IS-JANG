f14b0ff6c074f2ced7510a81e4bd9ca16fc07536
JIS2850 <isjang@8pin.dooray.com>
2022-11-14 10:38:22 +0900

    STUDY8-11

---
+++ sample1/family.py
@@ -0,0 +1,14 @@
+"""
+촌수를 계산하는 모듈입니다.
+"""
+
+def input ():
+    num = int(input()) # 전체 사람 수
+    target = list(map(int, input().split(' '))) # 두 사람의 촌수를 구할것
+    m = int(input()) # 관계 수
+
+
+
+
+if __name__ == '__main__':
+    input()
